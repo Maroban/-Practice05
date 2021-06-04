dfd568bec62423b3597c6cc9bc3e88052aa30080
hi <hi@DESKTOP-EGCI8BH>
2021-06-04 16:32:56 +0900

    ex04 완료

---
+++ src/com/javaex/ex04/Base.java
@@ -0,0 +1,18 @@
+package com.javaex.ex04;
+
+public class Base {
+
+	public void service(String state) {
+		switch (state) {
+		case "낮":
+			System.out.println("낮에는 열심히 수업듣자");
+			break;
+		case "밤":
+			System.out.println("밤에는 숙면");
+			break;
+		case "오후":
+			System.out.println("오후도 낮과 마찬가지로 공부해야 합니다.");
+			break;
+		}
+	}
+}
